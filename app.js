d773fcbbac056e7f6f88db15ffe735bd17385202
msaimsohail <116447889+msaimsohail@users.noreply.github.com>
2022-11-14 03:59:49 -0800

    hi

---
+++ app.js
@@ -0,0 +1,60 @@
+// window.alert("Thanks for your input");
+
+// alert("Welcome To JS Land ");
+
+// var book = "error! please enter valid password";
+// alert(book);
+
+
+
+// var a = "welcome to JS land Happy Coding";
+// alert(a);
+
+// var book = "hello... i can run JS through my web browser console";
+// alert(book);
+
+// var a = "username";
+// alert(a);
+
+// alert("Personal Details")
+// var username = +prompt("Enter Your username")
+// var Fathername = +prompt("Enter your Full Name")
+// var Fathername = +prompt("Enter your e-mail")
+// var Class = +prompt("Enter your message")
+
+
+// var age = +prompt("enter your age")
+
+
+// var Entername = +prompt("Enter Your username")
+// var maths = +prompt("physics marks")
+// var English = +prompt("Maths marks")
+// var Urdu = +prompt("Urdu marks")
+// var Computer = +prompt("English marks")
+// var Pst = +prompt("Pst marks")
+// var obtained = physics+maths+Urdu+English+Pst;
+// var totalmarks = 500 
+
+// var percentage = (obtained / totalmarks * 100)
+// console.log(percentage)
+
+// console.log(obtained / totalmarks * 100)
+
+// if (percentage > 50 && percentage < 60 ){
+//     console.log("D")
+// }
+// else if(percentage > 60 && percentage < 70){
+//     console.log("C")
+// }
+// else if(percentage > 70 && percentage < 80){
+//     console.log("B")
+// }
+// else if(percentage > 80 && percentage < 90){
+//     console.log("A")
+// }
+// else if(percentage > 90){
+//     console.log("A1")
+// }
+// else{
+//     console.log("failed")
+// }
